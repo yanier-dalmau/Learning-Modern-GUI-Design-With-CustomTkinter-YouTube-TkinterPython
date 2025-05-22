f193fa6c90673bb6a9c2e697fa31aafc3ab5a6ec
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 09:31:11 -0400

    Video 002. Agrega archivos de configuración y recursos al proyecto.
    - Se ha añadido el archivo .python-version para especificar la versión de Python utilizada (3.13.1).
    - Se ha incluido una imagen (codemy.png) para el icono de la aplicación.
    - Se ha modificado main.py para cargar el icono en formato PNG y se han realizado ajustes en el formato del código.
    - Se ha creado el archivo requirements.txt con las dependencias necesarias para el proyecto.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 import customtkinter
 
@@ -5,22 +6,23 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-# root = Tk()
 root = customtkinter.CTk()
 
 root.title('Tkinter.com - Custom Tkinter Buttons')
-root.iconbitmap('images/codemy.ico')
 root.geometry('600x350')
 
+icon_path = os.path.join(os.path.dirname(__file__), 'images', 'codemy.png')   # Construir ruta absoluta al archivo codemy.png
+icono = PhotoImage(file=icon_path)   # Cargar el icono en formato PNG
+
+root.iconphoto(True, icono)
 
 def hello():
-    my_label.configure(text = "Whatever")
+    my_label.configure(text="Whatever")
 
-# Use CTkButton instead of tkinter Button
-my_button = customtkinter.CTkButton(root, text = "Hello World!!!", command = hello)
-my_button.pack(pady = 80)
+my_button = customtkinter.CTkButton(root, text="Hello World!!!", command=hello)
+my_button.pack(pady=80)
 
-my_label = customtkinter.CTkLabel(root, text = "")
-my_label.pack(pady = 20)
+my_label = customtkinter.CTkLabel(root, text="")
+my_label.pack(pady=20)
 
 root.mainloop()
