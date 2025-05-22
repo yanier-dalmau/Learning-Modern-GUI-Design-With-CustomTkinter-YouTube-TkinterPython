001414fb24f2c43e640aa6f4c6b6d1d607bbcf2f
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 13:57:24 -0400

    Video 003. Establecer código simple de inicio

--- main.py
+++ main.py
@@ -16,26 +16,4 @@ icono = PhotoImage(file=icon_path)   # Cargar el icono en formato PNG
 
 root.iconphoto(True, icono)
 
-def hello():
-    my_label.configure(text=my_button.cget("text"))
-
-my_button = customtkinter.CTkButton(root, 
-    text="Hello World!!!", 
-    command=hello,
-    height=100,
-    width=200,
-    font=("Helvetica", 24),
-    text_color="black",
-    fg_color="red",
-    hover_color="green",
-    corner_radius=50,
-    bg_color="white",
-    border_width=10,
-    border_color="yellow",
-    state="normal")
-my_button.pack(pady=80)
-
-my_label = customtkinter.CTkLabel(root, text="")
-my_label.pack(pady=20)
-
 root.mainloop()
