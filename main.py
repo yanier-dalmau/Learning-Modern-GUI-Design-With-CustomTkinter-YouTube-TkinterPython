bd203a712ec433528b0849f4c58921f8546e7dcd
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 19:02:54 -0400

    Video 004. Establecer código simple de inicio

--- main.py
+++ main.py
@@ -23,37 +23,6 @@ else:
             'codemy.png')
         )
     )  # Usar icono PNG en otros sistemas operativos
-    
-def submit():
-    my_label.configure(text=f'Hello {my_entry.get()}')
-    my_entry.configure(state="disabled")
-
-def clear():
-    my_entry.configure(state="normal")
-    my_entry.delete(0, END)
-
-my_label = customtkinter.CTkLabel(root, text="", font=("Helvetica", 24))
-my_label.pack(pady=40)
-
-my_entry = customtkinter.CTkEntry(
-    root, 
-    placeholder_text="Enter you Name",
-    height=50,
-    width=200,
-    font=("Helvetica", 18),
-    corner_radius=50,
-    text_color="green",
-    placeholder_text_color="darkblue",
-    fg_color=("blue", "lightblue"), # outer, inner
-    state="normal"
-)
-my_entry.pack(pady=20)
-
-my_button = customtkinter.CTkButton(root, text="Submit", command=submit)
-my_button.pack(pady=10)
-
-clear_button = customtkinter.CTkButton(root, text="Clear", command=clear )
-clear_button.pack(pady=10)
 
 
 root.mainloop()
