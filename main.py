574130006eff5cd3f5ef0525ab66b6f708d586df
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 20:22:24 -0400

    Video 004. feat: Actualizar la lógica del juego según el estado del checkbox
    - Modificar la función game para mostrar un mensaje diferente según si el checkbox está seleccionado o no.

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ else:
     )  # Usar icono PNG en otros sistemas operativos
     
 def game():
-    my_label.configure(text="You clicked the thing!")
-
+    if check_var.get() == "on":
+        my_label.configure(text="You clicked the thing!")
+    else:
+        my_label.configure(text="You didn't clicked the thing!")
+        
+    
 check_var = customtkinter.StringVar(value="off")
 my_check = customtkinter.CTkCheckBox(root, 
     text="Would you like to play a game?",
@@ -35,6 +39,7 @@ my_check = customtkinter.CTkCheckBox(root,
     offvalue="off"
 )
 my_check.pack(pady=40)
+
 
 my_button = customtkinter.CTkButton(root, text="Submit", command=game)
 my_button.pack(pady=20)
