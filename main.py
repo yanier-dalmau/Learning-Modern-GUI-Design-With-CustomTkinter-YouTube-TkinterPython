eccdc2aaf49bcef0ba8a9d52919419dfd74fa63f
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 22:33:37 -0400

    Video 005. feat: Añadir botón para seleccionar color y actualizar etiqueta
    - Implementar la función `color_picker2` para actualizar la etiqueta de salida con el color seleccionado del combobox al presionar un botón.
    - Crear un botón que llame a `color_picker2` para facilitar la selección del color.

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ else:
 def color_picker(choice):
     output_label.configure(text=choice, text_color=choice)
 
+def color_picker2():
+    output_label.configure(text=my_combo.get(), text_color=my_combo.get())
 
 my_label = customtkinter.CTkLabel(root, text="Pick a color", font=("Helvetica", 18))
 my_label.pack(pady=40)
@@ -35,9 +37,14 @@ my_label.pack(pady=40)
 # Set the options for our combobox 
 colors = ["Red", "Green", "Blue"]
 # Create combobox
-my_combo = customtkinter.CTkComboBox(root, values=colors, command=color_picker)
+my_combo = customtkinter.CTkComboBox(root, values=colors)
 my_combo.pack(pady=0)
 
+# Create a button
+my_button = customtkinter.CTkButton(root, text="Pick a color", command=color_picker2)
+my_button.pack(pady=20)
+
+# Create output label
 output_label = customtkinter.CTkLabel(root, text="", font=("Helvetica", 18))
 output_label.pack(pady=20)
 
