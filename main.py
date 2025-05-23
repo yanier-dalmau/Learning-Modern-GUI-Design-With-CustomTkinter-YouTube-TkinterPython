9cc87e5eea764fa9a00da34acad41bffe48d132f
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-22 21:31:11 -0400

    Video 005. feat: Añadir etiqueta y combobox para selección de colores
    - Crear una etiqueta que indique "Pick a color" en la interfaz.
    - Implementar un combobox con opciones de colores: "Red", "Green" y "Blue".

--- main.py
+++ main.py
@@ -24,5 +24,13 @@ else:
         )
     )  # Usar icono PNG en otros sistemas operativos
 
+my_label = customtkinter.CTkLabel(root, text="Pick a color", font=("Helvetica", 18))
+my_label.pack(pady=40)
+
+# Set the options for our combobox 
+colors = ["Red", "Green", "Blue"]
+# Create combobox
+my_combo = customtkinter.CTkComboBox(root, values=colors)
+my_combo.pack(pady=20)
 
 root.mainloop()
