7f3716669e69bb6e42b0d7573b95fe25d061e3f0
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-24 17:59:18 -0400

    Video 007. Radio Buttons in CustomTkinter - Tkinter CustomTkinter 7

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), da
 root = customtkinter.CTk()
 
 root.title('Tkinter.com - Custom Tkinter Buttons')
-root.geometry('700x350')
+root.geometry('600x350')
 
 # Verificar el sistema operativo
 if platform.system() == 'Windows':
@@ -23,49 +23,6 @@ else:
             'codemy.png')
         )
     )  # Usar icono PNG en otros sistemas operativos
-
-
-def clicker():
-    my_progressbar.step()
-    my_label.configure(text=(int(my_progressbar.get()*100)))
-    
-def start():
-    my_progressbar.start()
-
-def stop():
-    my_progressbar.stop()
-
-
-
-my_progressbar = customtkinter.CTkProgressBar(root, 
-    width=300,
-    height=50,
-    corner_radius=50,  
-    border_width=5,     
-    border_color="red",
-    fg_color="green",
-    progress_color="pink",                                      
-    orientation="horizontal",
-    mode="determinate",
-    determinate_speed=5,
-    indeterminate_speed=5,
-)
-my_progressbar.pack(pady=40)
-
-# Set the default progess starting point
-my_progressbar.set(0)
-
-my_button = customtkinter.CTkButton(root, text="Click me", command=clicker)
-my_button.pack(pady=10)
-
-start_button = customtkinter.CTkButton(root, text="Start", command=start)
-start_button.pack(pady=10)
-
-stop_button = customtkinter.CTkButton(root, text="Stop", command=stop)
-stop_button.pack(pady=10)
-
-my_label = customtkinter.CTkLabel(root, text="", font=("Helvetica", 18))
-my_label.pack(pady=10)
 
 
 root.mainloop()
