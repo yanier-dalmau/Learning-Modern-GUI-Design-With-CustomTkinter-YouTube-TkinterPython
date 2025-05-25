0e5eefb98d6c8c1560d4c000e9676bebfefb73a1
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-25 18:23:34 -0400

    Video 010. Sliders - Tkinter CustomTkinter 10

--- main.py
+++ main.py
@@ -25,38 +25,4 @@ else:
     )  # Usar icono PNG en otros sistemas operativos
 
 
-# Create our function
-def clicker(value):
-    my_label.configure(text=f'Hello {value}')
-
-# Our button values
-my_values = ["Jhon", "April", "Wes"]
-# Create the button
-my_seg_button = customtkinter.CTkSegmentedButton(root, 
-    values=my_values, 
-    command=clicker,
-    width=300,
-    height=100,
-    font=("Helvetica", 58),
-    corner_radius=3,
-    border_width=5,
-    fg_color="red",
-    selected_color="green",
-    selected_hover_color="purple",
-    unselected_color="pink",
-    unselected_hover_color="orange",
-    text_color="yellow",
-    state="normal",
-    text_color_disabled="blue",
-    dynamic_resizing=True,
-)
-my_seg_button.pack(pady=40)
-
-# Set deafult Selection
-# my_seg_button.set("Jhon")
-
-# Label
-my_label = customtkinter.CTkLabel(root, text="", font=("Helvetica", 18))
-my_label.pack(pady=20)
-
 root.mainloop()
