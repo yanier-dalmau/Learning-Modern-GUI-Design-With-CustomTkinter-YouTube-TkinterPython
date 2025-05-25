74f8591b438297669813d8f3c097c4617ee2ffc4
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-25 17:03:35 -0400

    Video 009. Segmented Buttons - Tkinter CustomTkinter 9

--- main.py
+++ main.py
@@ -25,27 +25,4 @@ else:
     )  # Usar icono PNG en otros sistemas operativos
 
 
-# Create a scrollable frame
-my_frame = customtkinter.CTkScrollableFrame(root,
-    orientation="vertical",
-    width=300,
-    height=200,
-    label_text="Hello World!",
-    label_fg_color="blue",
-    label_text_color="yellow",
-    label_font=("Helvetica", 18),
-    label_anchor="center", # "w", "n", "ne", "e", "se", "s", "sw", "w", "nw", center
-    border_width=3,
-    border_color="green",
-    fg_color="red",
-    scrollbar_fg_color="yellow",
-    scrollbar_button_color="pink",
-    scrollbar_button_hover_color="blue",
-    corner_radius=20,
-)
-my_frame.pack(pady=40)
-
-for x in range(20):
-    customtkinter.CTkButton(my_frame, text="This is a button!!").pack(pady=10)
-
 root.mainloop()
