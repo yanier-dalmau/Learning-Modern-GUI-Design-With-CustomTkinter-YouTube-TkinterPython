347b86d4acbac81d783fb8d53d6f39e893bec9c7
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-26 08:43:45 -0400

    Video 012. The TabView Widget - Tkinter CustomTkinter 12

--- main.py
+++ main.py
@@ -25,49 +25,4 @@ else:
     )  # Usar icono PNG en otros sistemas operativos
 
 
-# Create funtion
-def switcher():
-    my_label.configure(text=switch_var.get())
-
-# Create toggle function
-def clicker():
-    # my_switch.deselect()
-    # my_switch.select()
-    my_switch.toggle()
-    
-
-# Create a StringVar
-switch_var = customtkinter.StringVar(value="on")
-# Create Switch
-my_switch = customtkinter.CTkSwitch(root, 
-    text="Switch", 
-    command=switcher,
-    variable=switch_var,
-    onvalue="on",
-    offvalue="off",
-    # width=200,
-    # height=100,
-    switch_width=200,
-    switch_height=25,
-    # corner_radius=15,
-    border_color="orange",
-    border_width=5,
-    fg_color="red",
-    progress_color="green",
-    button_color="pink",
-    button_hover_color="yellow",
-    font=("Helvetica", 24),
-    text_color="blue",
-    state="normal",
-)
-my_switch.pack(pady=40)
-
-# Create a label
-my_label = customtkinter.CTkLabel(root, text="")
-my_label.pack(pady=10)
-
-# Create a button
-my_button = customtkinter.CTkButton(root, text="Click Me!", command=clicker)
-my_button.pack(pady=10)
-
 root.mainloop()
