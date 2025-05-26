b5af6340c9c516d15221acee6ce7c79bca602c59
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-26 14:24:24 -0400

    Video 012. feat: Añadir radio de esquina al Tabview en la interfaz gráfica
    - Establecer 'corner_radius' en 10 para suavizar las esquinas del Tabview y mejorar su apariencia.

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ else:
 # Create Tabview
 my_tab = customtkinter.CTkTabview(root, 
     width=600,
-    height=250,                                  
+    height=250,
+    corner_radius=10,                                  
 )
 my_tab.pack(pady=10)
 
