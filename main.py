7a6b281ee4373931ed56447524fc3cf8ec95f49c
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-27 10:36:46 -0400

    Video 015. feat: Restringir el tamaño de la nueva ventana.
    - Hacer que la nueva ventana no sea redimensionable para mantener un diseño fijo.

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def new():
     new_window = customtkinter.CTkToplevel(root)
     new_window.title("This is a new window!")
     new_window.geometry("400x200")
+    new_window.resizable(False, False) # width, height
 
 
 my_button = customtkinter.CTkButton(root, text="Open New Window", command=new)
