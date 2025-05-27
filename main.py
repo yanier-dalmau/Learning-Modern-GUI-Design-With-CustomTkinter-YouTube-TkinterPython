6d333f3656d342545ebb70a856b508fccd46258b
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-05-27 09:54:48 -0400

    Video 015. New Top Level Windows - Tkinter CustomTkinter 15

--- main.py
+++ main.py
@@ -25,32 +25,4 @@ else:
     )  # Usar icono PNG en otros sistemas operativos
 
 
-# Create a Function
-def input():
-    dialog = customtkinter.CTkInputDialog(
-        text="What is your name?", 
-        title="Hello There!",
-        fg_color="white",
-        button_fg_color="red",
-        button_hover_color="pink",
-        button_text_color="black",
-        entry_fg_color="green",
-        entry_border_color="red",
-        entry_text_color="black",
-)
-    thing = dialog.get_input()
-    if thing:
-        my_label.configure(text=f"Hello {thing}")
-    else:
-        my_label.configure(text=f"Yout Forgot To Type Anything!")
-
-# Create a Button
-my_button = customtkinter.CTkButton(root, text="Click Me!", command=input)
-my_button.pack(pady=40)
-
-# Create a Label
-my_label = customtkinter.CTkLabel(root, text='')
-my_label.pack(pady=10)
-
 root.mainloop()
-
