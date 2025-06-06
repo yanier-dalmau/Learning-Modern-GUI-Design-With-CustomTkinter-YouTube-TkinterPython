99d5f5dcd84df532228ba4fe75c049afe6270483
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-06-05 22:29:40 -0400

    Video 020. feat:  Regresar al código del commit  fc4f8dc0 y establecer tema "green".

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from tkinter import *
 import customtkinter
 
 # Set the theme and color options
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
 root = customtkinter.CTk()
 
 root.title('Tkinter.com - Custom Tkinter Buttons')
-root.geometry('700x350')
+root.geometry('700x500')
 
 # Verificar el sistema operativo
 if platform.system() == 'Windows':
@@ -24,5 +24,37 @@ else:
         )
     )  # Usar icono PNG en otros sistemas operativos
 
+
+
+mode = "dark"
+def change():
+    global mode
+    if mode == "dark":
+        customtkinter.set_appearance_mode("light")
+        mode = "light"
+        # Clear text box
+        my_text.delete(0.0, END)
+        my_text.insert(END, "This is Light Mode...")
+    else:
+        customtkinter.set_appearance_mode("dark")
+        mode = "dark"
+        # Clear text box
+        my_text.delete(0.0, END)
+        my_text.insert(END, "This is Dark Mode...")
+
+
+def change_colors(choice):
+    customtkinter.set_default_color_theme(choice)
+
+
+my_text = customtkinter.CTkTextbox(root, width=600, height=300)
+my_text.pack(pady=20)
+
+my_button = customtkinter.CTkButton(root, text="Change Light/Dark", command=change)
+my_button.pack(pady=20)
+
+colors = ["blue", "dark-blue", "green"]
+my_option = customtkinter.CTkOptionMenu(root, values=colors, command=change_colors)
+my_option.pack(pady=10)
 
 root.mainloop()
