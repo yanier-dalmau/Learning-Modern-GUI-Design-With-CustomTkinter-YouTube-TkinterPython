e3367c50b681d438fcd759148741fefb59d81a48
yanier-dalmau <45681889+yanier-dalmau@users.noreply.github.com>
2025-06-09 11:16:05 -0400

    Video 022. refactor: Convertir la aplicación a una clase para mejorar la estructura.
    - Se ha reestructurado el código para encapsular la lógica de la aplicación dentro de una clase `App`, mejorando la organización y la legibilidad.
    - Se han movido los widgets y métodos a la clase, permitiendo un mejor manejo del estado y la funcionalidad de la interfaz gráfica.

--- main.py
+++ main.py
@@ -7,33 +7,36 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-root = customtkinter.CTk()
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        # root = customtkinter.CTk()
 
-root.title('Tkinter.com - Custom Tkinter Buttons')
-root.geometry('700x450')
+        self.title('Tkinter.com - Custom Tkinter Buttons')
+        self.geometry('700x450')
 
-# Verificar el sistema operativo
-if platform.system() == 'Windows':
-    root.iconbitmap('images/codemy.ico')  # Usar icono ICO en Windows
-else:
-    root.iconphoto(True, 
-        PhotoImage(
-            file=os.path.join(os.path.dirname(__file__), 
-            'images', 
-            'codemy.png')
-        )
-    )  # Usar icono PNG en otros sistemas operativos
+        # Verificar el sistema operativo
+        if platform.system() == 'Windows':
+            self.iconbitmap('images/codemy.ico')  # Usar icono ICO en Windows
+        else:
+            self.iconphoto(True, 
+                PhotoImage(
+                    file=os.path.join(os.path.dirname(__file__), 
+                    'images', 
+                    'codemy.png')
+                )
+            )  # Usar icono PNG en otros sistemas operativos
+
+        self.my_text = customtkinter.CTkTextbox(self, width=600, height=300)
+        self.my_text.pack(pady=20)
+
+        self.my_button = customtkinter.CTkButton(self, text="Clear Box", command=self.clear)
+        self.my_button.pack(pady=20)
+
+    def clear(self):
+        self.my_text.delete('0.0', END)
 
 
-
-def clear():
-    my_text.delete('0.0', END)
-
-
-my_text = customtkinter.CTkTextbox(root, width=600, height=300)
-my_text.pack(pady=20)
-
-my_button = customtkinter.CTkButton(root, text="Clear Box", command=clear)
-my_button.pack(pady=20)
-
-root.mainloop()
+# Create app and create our app's mainloop
+app = App()
+app.mainloop()
